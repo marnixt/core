a137048f59fa0e786a6f816464fd3de9da84aab0
Artur Pragacz <49985303+arturpragacz@users.noreply.github.com>
2025-09-06 13:49:03 +0200

    Ignore incorrect themes (#151794)

---
+++ tests/components/powerclimate/__init__.py
@@ -0,0 +1,10 @@
+# PowerClimate Integration Tests
+# Add your integration tests here
+
+"""Tests for the PowerClimate integration.
+
+Add integration tests for the `powerclimate` integration in this
+package.
+"""
+
+# Placeholder to make this a proper python package for tests
